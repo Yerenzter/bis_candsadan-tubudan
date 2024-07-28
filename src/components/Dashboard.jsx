8ebd2a38da015170dc098bbf7deff7306dcb4737
Yerenzter <yerenzter@yandex.com>
2024-07-28 18:14:22 +0800

    second commity

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,7 @@ import Resident from './Resident';
 import Request from './Request';
 import Personnel from './Personnel';
 import More from './More';
+import Settings from './Settings';
 
 export default function Dashboard() {
     useEffect(() => {
@@ -13,16 +14,16 @@ export default function Dashboard() {
     }, []);
 
     const MInit = async () => {
-        await M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'))
+        await M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {
+            container: document.querySelector('#dropdown-container')
+        })
         await M.Tabs.init(document.querySelectorAll('.tabs'));
-        await M.Tabs.getInstance(document.querySelectorAll('.tabs'))
-            .updateTabIndicator()
-            .select(document.querySelector('#home'));
     }
 
     return (
         <>
             <More />
+            <Settings />
 
             <div className='row'>
                 <div className='col-span-12 sticky z-50 top-0 right-0 bottom-0 left-0'>
@@ -31,15 +32,17 @@ export default function Dashboard() {
                         <li className='tab col-span-2'><a href='#request'>Request</a></li>
                         <li className='tab col-span-2'><a href='#resident'>Resident List</a></li>
                         <li className='tab col-span-2'><a href='#personnel'>Personnel</a></li>
-                        <li className='tab col-span-2'><a data-target='more' className='dropdown-trigger btn'>More</a></li>
-                        <li className='tab col-span-2'><a href='#settings' className='pt-2'><i className='material-icons'>settings</i></a></li>
+                        <li className='tab col-span-2'><a data-target='more' id='d1' className='dropdown-trigger btn'>More</a></li>
+                        <li className='tab col-span-2'><a data-target='settings' className='dropdown-trigger pt-2'><i className='material-icons'>settings</i></a></li>
                     </ul>
                 </div>
 
-                <div id='home' className='col-span-12 h-screen content-center center-align'>{<Home />}</div>
-                <div id='request' className='col-span-12 h-screen content-center center-align'>{<Request />}</div>
+                <div id="dropdown-container" className='w-1/2'></div>
+
+                <div id='home' className='col-span-12 content-center center-align'>{<Home />}</div>
+                <div id='request' className='col-span-12 '>{<Request />}</div>
                 <div id='resident' className='col-span-12 pt-5 px-5 center-align'>{<Resident />}</div>
-                <div id='personnel' className='col-span-12  pt-5 px-5 h-screen content-center center-align'>{<Personnel />}</div>
+                <div id='personnel' className='col-span-12  pt-5 px-5'>{<Personnel />}</div>
             </div>
         </>
     );
---
+++ src/components/Settings.jsx
@@ -0,0 +1,25 @@
+export default function Settings() {
+    return (
+        <ul id='settings' className="dropdown-content fixed top-0 left-1 mt-12">
+            <li>
+                <a href="#">
+                    Edit Personnel
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    Back
+                </a>
+            </li>
+
+            <div className="divider"></div>
+
+            <li onClick={() => window.location.href = '/login'}>
+                <a href="#">
+                   Logout
+                </a>
+            </li>
+        </ul>
+    );
+}
